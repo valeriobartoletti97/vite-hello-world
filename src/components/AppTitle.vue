02a7fbf4c6114e3b14ff4905a4470f618c428641
valeriobartoletti97 <valerio.bartoletti97gmail.com>
2023-11-09 01:39:10 +0100

    added AppTitle component

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+        <h1>{{title}}</h1>
+    </main>
+</template>
+
+<script>
+    export default {
+        name:'AppTitle',
+        data(){
+           return{
+            title: 'Hello World'
+           }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
